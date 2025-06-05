e7f75c7c0023c7508e105e4f06b626055249d72c
Pedro Matos <pmattos.games@gmail.com>
2025-06-05 17:12:24 -0300

    Add files via upload

---
+++ aula01.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int idade = 25;
+    float altura = 1.75;
+    char letra = 'a';
+
+    printf("idade: %d\n", idade);
+    printf("altura %.2f\n", altura);
+    printf("letra %C\n", letra);
+
+    return 0;
+}
