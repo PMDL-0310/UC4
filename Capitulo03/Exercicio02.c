d2101c67aef830a87ecd5d7eb6c1715610b234de
Pedro Matos <pmattos.games@gmail.com>
2025-06-12 17:29:34 -0300

    Add files via upload

---
+++ Capitulo03/Exercicio02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, num3;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%f", &num2);
+
+    printf("Digite o terceiro número: ");
+    scanf("%f", &num3);
+
+    float maior = num1;
+
+    if (num2 > maior) {
+        maior = num2;
+    }
+
+    if (num3 > maior) {
+        maior = num3;
+    }
+
+    printf("O maior número digitado é: %.2f\n", maior);
+
+    return 0;
+}
